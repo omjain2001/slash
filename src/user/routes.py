0c361a900f2148225ca6cab3830ee1f21050fd34
Sakshi Mahadik <sakshimahadik2511@gmail.com>
2023-11-28 02:27:54 -0500

    final commit

--- src/user/routes.py
+++ src/user/routes.py
@@ -2,35 +2,40 @@ from flask import Flask, request
 from src.app import app, login_required
 from src.user.models import User
 
-@app.route('/user/signup/', methods = ['POST'])
+
+@app.route('/user/signup/', methods=['POST'])
 def signup():
     return User().signup()
 
-@app.route('/user/login/', methods = ['POST'])
+
+@app.route('/user/login/', methods=['POST'])
 def login():
     return User().login()
+
 
 @app.route('/user/signout/')
 def signout():
     return User().signout()
 
-@app.route('/user/getProfile/', methods = ['GET'])
+
+@app.route('/user/getProfile/', methods=['GET'])
 @login_required
 def getProfile():
     return User().getProfile()
 
-@app.route('/user/addProduct/', methods = ['POST'])
+
+@app.route('/user/addProduct/', methods=['POST'])
 @login_required
 def addProduct():
     product = {
         "title": request.args.get('title'),
         "rating": request.args.get('rating'),
         "imgSrc": request.args.get('imgSrc'),
-		"marketplace": request.args.get('marketplace'),
-		"price": request.args.get('price'),
-		"currency": request.args.get('currency'),
-		"productURL": request.args.get('productURL'),
-		"noOfRatings": request.args.get('noOfRatings'),
-		"paymentMode": request.args.get('paymentMode')
+        "marketplace": request.args.get('marketplace'),
+        "price": request.args.get('price'),
+        "currency": request.args.get('currency'),
+        "productURL": request.args.get('productURL'),
+        "noOfRatings": request.args.get('noOfRatings'),
+        "paymentMode": request.args.get('paymentMode')
     }
     return User().addProduct(product)
